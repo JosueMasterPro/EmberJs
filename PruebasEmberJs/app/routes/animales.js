c9d4a96a152e3802b12c7eb99bf38346874bcbf9
Josue Reyes <josuereyes1229@gmail.com>
2022-10-06 12:12:32 -0600

    Nuevas Carpeta y funciones
    Avance en mi carpeta de tesstember, y añadi la carpeta donde esta pero no sirve, solo es para vercodigo por si usa una version distinta xD

--- PruebasEmberJs/app/routes/animales.js
+++ PruebasEmberJs/app/routes/animales.js
@@ -1,7 +1,12 @@
 import Route from '@ember/routing/route';
 
 export default class AnimalesRoute extends Route {
-  model() {
-    return ['Halley', 'Merlin','Fran', 'josue','wifi'];
-  }
+    prueba(){
+        return ['Juan', 'Pedro','Perez', 'Pablo','wifi'];
+    }
+    model() {
+        return ['Halley', 'Merlin','Fran', 'josue','wifi'];
+    };
+  
 }
+
