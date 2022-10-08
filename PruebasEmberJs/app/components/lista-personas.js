d582d7e1475c0565bbe332bd7740346822b4b62d
Josue Reyes <josuereyes1229@gmail.com>
2022-10-08 14:06:12 -0600

    #3

--- PruebasEmberJs/app/components/lista-personas.js
+++
@@ -1,9 +0,0 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-
-export default class ListaPersonasComponent extends Component {
-  @action
-  showPerson(personas) {
-    alert(`Su Nombre es ${personas}!`);
-  }
-}
