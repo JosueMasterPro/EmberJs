2925c352ed7af709ab6cd689eae965fbecd3cbc6
Josue Reyes <josuereyes1229@gmail.com>
2022-10-22 18:44:30 -0600

    Buscar por categoria y nombre

--- PaginaWeb/app/controllers/index.js
+++ PaginaWeb/app/controllers/index.js
@@ -8,25 +8,45 @@ export default class IndexController extends Controller {
 
     Pro1 = this.store.peekAll('product');
     @tracked datos=this.Pro1;
-    filterItems = (query) => {
-        return this.Pro1.filter(
+    @tracked Filtros = this.Pro1;
+    search = '';
+    cod = '';
+
+    filterItemsbyName = (Data,query) => {
+        return Data.filter(
           (el) => el.name.toLowerCase().indexOf(query.toLowerCase()) > -1
         );
     };
-    search = '';
-    search = document.querySelector('#search');
-
-    get Product(){
-        this.datos = this.filterItems(search.value);
+    filterItemsbyCategory = (Data,query) => {
+        return Data.filter(
+          (el) => el.category.toLowerCase().indexOf(query.toLowerCase()) > -1
+        );
+    };
+        search = document.querySelector('#search');
+        cod = document.getElementById("category");
+        
+    get Product(){ 
         return this.datos;
     }
 
     @action
     filter() {
-        
-        console.log(search.value);
-        console.log(this.Product);
-        this.Product;
+        this.ComboBox();
+
+    }
+    @action
+    ComboBox() { 
+        this.cod = document.getElementById("category");
+        this.datos = this.Pro1;
+        this.Filtros = this.Pro1;
+        if(this.cod.value == 0){
+            this.Filtros = this.filterItemsbyCategory(this.datos,'');
+            this.datos = this.filterItemsbyName(this.Filtros,search.value);
+        }
+        else{
+            this.Filtros = this.filterItemsbyCategory(this.datos,this.cod.value);
+            this.datos = this.filterItemsbyName(this.Filtros,search.value);
+        }
     }
 
 }
