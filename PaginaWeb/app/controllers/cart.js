7c55470614b324ad53094d744114ad8c01b22307
Josue Reyes <josuereyes1229@gmail.com>
2022-11-01 18:09:33 -0600

    Arreglando El Carrito
    Se arreglo el numero que quedaba como text, y se puso os botones para agregar carrito

--- PaginaWeb/app/controllers/cart.js
+++ PaginaWeb/app/controllers/cart.js
@@ -4,7 +4,7 @@ import { action } from '@ember/object';
 
 export default class CartController extends Controller {
   @service('shopping-cart') cart;
-
+  cod = '';
   get subtotal() {
     return this.cart.itemList.reduce((acc, item) => {
       return acc + item.price * item.count;
@@ -22,8 +22,30 @@ export default class CartController extends Controller {
   @action
   updateItemCount(item, event) {
     const count = event.target.value;
+    console.log(count);
     if (count >= 0) {
       item.count = count;
+    } else {
+      item.count = 0;
+    }
+  }
+
+  cod = document.getElementById("input1");
+  Suma = 1;
+  @action
+  ItemCount(item, event) {
+    this.cod = document.getElementById("input-" + item.name + "-"+item.color);
+    if (this.cod.value >= 0) {
+      item.count = parseFloat(this.cod.value) + parseFloat(this.Suma)
+    } else {
+      item.count = 0;
+    }
+  }
+  @action
+  ItemCountless(item, event) {
+    this.cod = document.getElementById("input-" + item.name + "-"+item.color);
+    if (this.cod.value >= 1) {
+      item.count = parseFloat(this.cod.value) - parseFloat(this.Suma)
     } else {
       item.count = 0;
     }
